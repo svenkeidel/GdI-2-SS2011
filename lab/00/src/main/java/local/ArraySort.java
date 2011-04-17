5166d3fd644ca4f20a0cbfc34070d169e2d02cb2
Sven Keidel <svenkeidel@gmail.com>
2011-04-17 23:33:56 +0200

    worked on lab 0

---
+++ lab/00/src/main/java/local/ArraySort.java
@@ -0,0 +1,87 @@
+package local;
+
+import java.util.Arrays;
+
+/**
+ * provides methods to insert an element in a sorted array.
+ * 
+ * @author Jakob Karolus, Kevin Munk
+ * @version 1.0
+ * 
+ */
+public class ArraySort {
+
+	/**
+	 * inserts a number into a sorted array of numbers.
+	 * 
+	 * @param element
+	 *            the element to insert.
+	 * @param array
+	 *            the sorted array.
+	 * @return the sorted array with the element at the right position.
+	 */
+	public static int[] insertElement(int element, int[] array) {
+		// Copies the array and increases the length of the new array
+		int[] tempArray = Arrays.copyOf(array, array.length + 1);
+		// Insert the element at the last position of the new array
+		tempArray[tempArray.length - 1] = element;
+		// Sort the array
+		Arrays.sort(tempArray);
+
+		return tempArray;
+	}
+
+	/**
+	 * inserts a number into a sorted array of numbers.
+	 * 
+	 * @param element
+	 *            the element to insert.
+	 * @param array
+	 *            the sorted array.
+	 * @return the sorted array with the element at the right position.
+	 */
+	public static int[] insertElementFast(int element, int[] array) {
+
+		int length = array.length;
+
+		int[] temp = new int[length+1];
+
+		// if the element must be appended at the end
+		if(array[length-1] < element) {
+			System.arraycopy(array, 0, temp, 0, length);
+			temp[length] = element;
+
+		// if the element must be inserted at the beginning
+		} else if(array[0] > element) {
+			System.arraycopy(array, 0, temp, 1, length);
+			temp[0] = element;
+
+		} else {
+
+			int upper = length;
+			int lower = 0;
+			int pos   = 0;
+
+			while(upper >= lower) {
+				pos = (upper + lower) / 2;
+
+				// if at the right position: break
+				if((pos >= length) || (pos == 0) || ((array[pos-1] <= element) && (array[pos] >= element))) 
+					break;
+
+				// if number to big go right
+				if(array[pos] < element)
+					lower = pos + 1;
+				// if number to low go left
+				else if(array[pos - 1] > element)
+					upper = pos;
+			}
+
+			System.arraycopy(array, 0, temp, 0, pos);
+			System.arraycopy(array, pos, temp, pos + 1, length - pos);
+			temp[pos] = element;
+		}
+
+		return temp;
+	}
+}
---
+++ lab/00/src/test/java/local/ArraySortTest.java
@@ -0,0 +1,88 @@
+package local;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.Arrays;
+
+import local.ArraySort;
+
+
+/**
+ * tests the methods of the class ArraySort.
+ * Use these tests to test your insertElementFast method.
+ * 
+ * @author Jakob Karolus, Kevin Munk
+ * @version 1.0
+ *
+ */
+public class ArraySortTest {
+
+	private int[] array;
+	
+	/**
+	 * initiate the array
+	 */
+	@Before
+	public void init(){
+		array = new int[]{1,2,3,4,5};
+	}
+	
+	/**
+	 * Inserts a Element that will be put at the front of the array.
+	 * Tests that the array will be the one it should be.
+	 */
+	@Test
+	public void insertAtFrontTest(){
+		this.array = ArraySort.insertElementFast(0, array);
+		Assert.assertArrayEquals(new int[]{0,1,2,3,4,5}, this.array);
+	}
+	
+	/**
+	 * Inserts a element that is already in the array.
+	 * Tests that the array will be the one it should be.
+	 */
+	@Test
+	public void insertSameInMiddleTest(){
+		this.array = ArraySort.insertElementFast(2, array);
+		Assert.assertArrayEquals(new int[]{1,2,2,3,4,5}, this.array);
+	}
+	
+	/**
+	 * Inserts a element that will be put in the back of the array.
+	 * Tests that the array will be the one it should be.
+	 */
+	@Test
+	public void insertAtBackTest(){
+		this.array = ArraySort.insertElementFast(42, array);
+		Assert.assertArrayEquals(new int[]{1,2,3,4,5,42}, this.array);
+	}
+	
+	public int[] createRandomArray(int length) {
+		Random r = new Random();
+		int[] temp = new int[length];
+		for(int i=0; i<length; i++)
+			temp[i] = r.nextInt(length);
+		Arrays.sort(temp);
+		return temp;
+	}
+
+	@Test
+	public void randomArrayTest() {
+		Random r = new Random();
+		int element;
+		int[] randomArray;
+		int[] testArray;
+		for(int i=1; i<=100; i++) {
+			randomArray = createRandomArray(i);
+			element = r.nextInt(11);
+
+			testArray = ArraySort.insertElementFast(element, randomArray);
+			this.array = ArraySort.insertElement(element, randomArray);
+
+			Assert.assertArrayEquals(testArray, this.array);
+		}
+	}
+}
