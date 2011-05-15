b7a4bb06864aafe137857eb45620392997e59e74
Sebastian Sztwiertnia <sebastian@sztwiertnia.de>
2011-05-15 17:17:28 +0200

    path looking changed

--- lab/02/src/main/java/gui/GridElementGUI.java
+++ lab/02/src/main/java/gui/GridElementGUI.java
@@ -26,7 +26,7 @@ public class GridElementGUI extends JButton {
 
 	Color o_green = new Color(85, 107, 47);
 	Color grid_color = new Color(101, 101, 101);
-	Color path_color = new Color(255, 127, 0);
+	Color path_color = new Color(255, 0, 0);
 	
 	Border path_Border = BorderFactory.createLineBorder(path_color, 1);
 	Border usual_Border = BorderFactory.createLineBorder(grid_color, 1);
@@ -113,7 +113,7 @@ public class GridElementGUI extends JButton {
 			break;
 		}
 		case PATH: {
-		//	this.setBackground(this.getBackground().darker());
+			this.setBackground(this.getBackground().darker());
 			this.setBorder(path_Border);
 			break;
 		}
