52d3c1fa29bc371d539da128e29e1fd20bc2b8c1
Sven Keidel <svenkeidel@gmail.com>
2011-05-21 15:05:57 +0200

    added empty method bodys to trees to reach compiling stage

--- lab/03/src/main/java/datamodel/tree/sequential/SequentialTree.java
+++ lab/03/src/main/java/datamodel/tree/sequential/SequentialTree.java
@@ -18,7 +18,126 @@ import datamodel.tree.TreeNode;
  */
 public class SequentialTree extends Tree {
 
-	//TODO: add unimplemented method of Tree class and attributes, if necessary
-	
+	/**
+	 * Moves to the left child of the current node
+	 * 
+	 * @return true if left child exists and the move was successful; otherwise
+	 *         false
+	 */
+	public boolean moveToLeftNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
 
+	/**
+	 * Moves to the right child of the current node
+	 * 
+	 * @return true if right child exists and the move was successful; otherwise
+	 *         false
+	 */
+	public boolean moveToRightNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * Moves to the parent of the current node
+	 * 
+	 * @return true if parent exists and the move was successful; otherwise
+	 *         false
+	 */
+	public boolean moveToParentNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * @return true if left child exists; otherwise false
+	 */
+	public boolean hasLeftNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * @return true if right child exists; otherwise false
+	 */
+	public boolean hasRightNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * @return true if parent exists; otherwise false
+	 */
+	public boolean hasParentNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * Sets the left child of the current node
+	 * 
+	 * @return true if successful; otherwise false (no root set)
+	 * 
+	 */
+	public boolean setLeftNode(TreeNode node) {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * Sets the right child of the current node
+	 * 
+	 * @return true if successful; otherwise false (no root set)
+	 * 
+	 */
+	public boolean setRightNode(TreeNode node) {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * Sets the current node. If the tree is empty, sets the root.
+	 * 
+	 */
+	public void setCurrentNode(TreeNode node) {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * @return the current node or null if the tree is empty
+	 */
+	public TreeNode getCurrentNode() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * moves to the root node of this tree
+	 * 
+	 * @return true if there's a root; otherwise false
+	 */
+	public boolean moveToRoot() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+
+	/**
+	 * clears the whole tree, which includes deleting all nodes and the root
+	 * node
+	 */
+	public void clearTree() {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
+	
+	/**
+	 * TODO: add Javadoc
+	 */
+	public boolean equals(Object o) {
+		//TODO: Implement me
+		throw new UnsupportedOperationException("Implement Me");
+	}
 }
